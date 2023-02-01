f83267456c1e6415dd0859acce10709b1ec3bf77
Kevin Scott <kdscott@masseyservices.com>
2023-02-01 16:27:45 -0500

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DatePickerExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatePickerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DatepickerModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Perform actions based on the selected date
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/DatepickerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatePickerExample.Models
+{
+    public class DatepickerModel
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Please select a date")]
+        public DateTime SelectedDate { get; set; }
+    }
+}
